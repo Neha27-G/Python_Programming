2f14599a36e992f4fb217b803bc7c06d9f826024
Neha27-G <niagupta8@gmail.com>
2026-07-04 23:05:46 +0530

    program to check the perfect Number

---
+++ Application Programs/program39.py
@@ -0,0 +1,31 @@
+def CheckPerfect(iNo):
+    iCnt = 0
+    iSum = 0
+
+    for iCnt in range(1,(iNo//2) + 1):   # To reduce time complexity(// means integer division)
+        if((iNo % iCnt)== 0):
+            iSum = iSum + iCnt
+
+        if(iSum > iNo):
+            break
+
+    if(iSum == iNo):
+        return True
+    else:
+        return False    
+ 
+def main():
+    iValue = 0
+    iRet = 0
+
+    iValue = int(input("Enter the Number :"))
+
+    iRet = CheckPerfect(iValue)
+
+    if(iRet == True):
+        print("It is Perfect Number")
+    else:
+        print("It is Not Perfect Number")
+        
+if __name__ =="__main__":
+    main()
