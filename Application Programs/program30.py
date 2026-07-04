dfc00e4334ce5f06db4f53bc18d67b5102053190
Neha27-G <niagupta8@gmail.com>
2026-07-04 23:02:35 +0530

    Used datatypes in python

---
+++ Application Programs/program30.py
@@ -0,0 +1,14 @@
+def main():
+    
+    sName = input("Enter Your Name :")      #python already returns a string
+
+    iAge = int(input("Enter Your Age :"))
+
+    fMarks = float(input("Enter Your Marks :"))
+
+    print("\nName :",sName)
+    print("\nAge :",iAge)
+    print("\nMarks :",fMarks)
+
+if __name__ =="__main__":
+    main()
