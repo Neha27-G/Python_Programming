3d67a935ba14a50e2e741ba339bb94952b74b728
Neha27-G <niagupta8@gmail.com>
2026-07-07 22:33:51 +0530

    program to print jump to 2 in numbers

---
+++ Application Programs/program40.py
@@ -0,0 +1,19 @@
+# Input : 5
+# Output : 1  3   5
+
+def Display(iNo):
+    iCnt = 0
+
+    for iCnt in range(1, iNo+1, 2):
+        print(iCnt, end = "\t")
+       
+def main():
+
+    iValue = 0
+
+    iValue = int(input("Enter the Number :"))
+
+    Display(iValue)
+    
+if __name__ == "__main__":
+    main()
