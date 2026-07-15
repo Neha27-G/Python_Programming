47bf99b7677efed626121dd247dc82ee9184da2d
Neha27-G <niagupta8@gmail.com>
2026-07-15 23:55:04 +0530

    program to print the numbers, n number of times

---
+++ Application Programs/program50.py
@@ -0,0 +1,12 @@
+def main():
+    iCnt = 0
+
+    iCnt = 1
+    while(iCnt <=5):
+        print(iCnt)
+        iCnt = iCnt+1
+    
+    print("\n")
+
+if __name__ == "__main__":
+    main()
---
+++ Application Programs/program51.py
@@ -0,0 +1,12 @@
+def main():
+    iCnt = 0
+
+    iCnt = 5
+    while(iCnt >=1):
+        print(iCnt)
+        iCnt = iCnt - 1
+    
+    print("\n")
+
+if __name__ == "__main__":
+    main()
