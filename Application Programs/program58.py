99683c92673b039894c801eafab0584ab9a91646
Neha27-G <niagupta8@gmail.com>
2026-07-22 11:13:45 +0530

    Program to count the Digits of given number

---
+++ Application Programs/program58.py
@@ -0,0 +1,24 @@
+def CountDigits(iNo):
+    iCount = 0
+
+    if(iNo == 0):
+        return 1
+    
+    if(iNo < 0):
+        iNo = -iNo
+
+    while(iNo != 0):
+        iDigits = iNo % 10
+        iCount = iCount + 1
+        iNo = iNo // 10
+    return iCount
+
+def main():
+
+    iValue = int(input("Enter the Number :"))
+
+    iRet = CountDigits(iValue)
+    print("Number of Digits :",iRet)
+
+if __name__ == "__main__":
+    main()
