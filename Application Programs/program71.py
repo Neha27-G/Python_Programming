cee857dee175248b44fbd69dc172b4969e6426c8
Neha27-G <niagupta8@gmail.com>
2026-07-30 23:32:21 +0530

    program based on array

---
+++ Application Programs/program71.py
@@ -0,0 +1,19 @@
+def main():
+
+    Arr = [0] * 7
+
+    Arr[0]= 10
+    Arr[3]= 20
+    Arr[6]= 30
+
+    print(len(Arr))
+
+    print(Arr[0])
+    print(Arr[3])
+    print(Arr[6])
+
+    print(Arr[2])        #garbage value
+    print(Arr[5])
+    
+if __name__ == "__main__":
+    main()
