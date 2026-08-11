3749390e9c0aa918c8b2d4b0b2c505c8f7a08555
Neha27-G <niagupta8@gmail.com>
2026-08-11 12:47:04 +0530

    program based on array

---
+++ Application Programs/program85.py
@@ -0,0 +1,11 @@
+def Display(Arr, Size):
+
+    for i in range(Size):
+        print(Arr[i])
+
+def main():
+    Brr = [10,20,30,40]  
+    Display(Brr, 4)
+    
+if __name__ == "__main__":
+    main()
