d1c9b85a821f1eafba670fdad2d9982518539b87
Neha27-G <niagupta8@gmail.com>
2026-08-18 23:57:24 +0530

    program on Linear search(Array)

---
+++ Application Programs/program100.py
@@ -0,0 +1,38 @@
+def LinearSearch(Arr, iSize , iNo):
+    iCnt = 0
+
+    for iCnt in range(iSize):
+
+        if(Arr[iCnt] == iNo):
+            break
+
+    if(iCnt == iSize-1 and Arr[iCnt] != iNo):
+        return False
+    else:
+        return True
+
+def main():
+    Brr = None
+    bRet = 0
+    iLength = int(input("Enter number of Elements :"))
+
+    Brr = [0] * iLength  #memory allocation
+
+    print("Enter the elements :")
+
+    for i in range(iLength):
+        Brr[i] = int(input())
+
+    iValue = int(input("Enter the Element that you want to search :\n"))
+    
+    bRet = LinearSearch(Brr, iLength, iValue)
+
+    if(bRet == True):
+        print("Element is present")
+    else:
+        print("Element is not present")
+
+    del Brr
+
+if __name__ == "__main__":
+    main()
